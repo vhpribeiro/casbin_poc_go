beca6932e094b3fedc21616ed65880a54e298ad3
Vitor Hugo Pereira Ribeiro <vitor.ribeiro@ambevtech.com.br>
2021-01-28 10:58:26 -0300

    Tirando arquivos que não eram relacionados ao casbin

--- entity/post.go
+++
@@ -1,7 +0,0 @@
-package entity
-
-type Post struct {
-	ID    int64  `json:"id" bson:"_id"`
-	Title string `json:"title" bson:"title"`
-	Text  string `json:"text" bson:"text"`
-}
--- server.go
+++ server.go
@@ -6,15 +6,11 @@ import (
 
 	"go_tutorial_post.com/controller"
 	router "go_tutorial_post.com/http"
-	"go_tutorial_post.com/repository"
 	"go_tutorial_post.com/service"
 )
 
 var (
 	httpRouter     router.IRouter             = router.NewChiRouter()
-	postRepository repository.IPostRepository = repository.NewMongoPostRepository()
-	postService    service.IPostService       = service.NewPostService(postRepository)
-	postController controller.IPostController = controller.NewPostController(postService)
 	userService    service.IUserService       = service.NewUserService()
 	userController controller.IUserController = controller.NewUserController(userService)
 )
@@ -24,10 +20,6 @@ func main() {
 	httpRouter.GET("/", func(response http.ResponseWriter, request *http.Request) {
 		fmt.Fprintln(response, "Up and Runnning")
 	})
-
-	httpRouter.GET("/posts", postController.GetPosts)
-
-	httpRouter.POST("/posts", postController.AddPosts)
 
 	httpRouter.GET("/users", userController.CheckIfUserHasPermission)
 
